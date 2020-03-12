276622a87e4cb31b563cf528a00e8a61abd208bb
alekorn <entkorn@gmail.com>
2020-03-12 15:53:41 +0300

    add arg_parse

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from page_loader.engine import run
+from page_loader.engine import run, arg_parse
 
 
 def main():
-    run()
+    output_dir, url = arg_parse()
+    print(output_dir, url, run())
 
 if __name__ == '__main__':
     main()

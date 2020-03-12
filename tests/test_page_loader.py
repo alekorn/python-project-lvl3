ecf151fa7fa75b6c8967df634a6effcbbde9e115
alekorn <entkorn@gmail.com>
2020-03-12 15:51:17 +0300

    add test_give_a_name

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,5 +1,27 @@
-from page_loader.engine import run
+from page_loader.engine import run, give_a_name
 
 
 def test_page_loader():
     assert run()  == 'Hello world'
+
+
+def test_give_a_name():
+    assert give_a_name('https://hexlet.io/courses')  == 'hexlet-io-courses.html'
+    assert give_a_name('http://hexlet.io/courses')  == 'hexlet-io-courses.html'
+    assert give_a_name('hexlet.io/courses')  == 'hexlet-io-courses.html'
+    assert give_a_name('https://hexlet.io/A1234567890')  == 'hexlet-io-A1234567890.html'
+
+
+
+# def test_page_loader():
+#     assert run()  == 'Hello world'
+# def test_page_loader():
+#     assert run()  == 'Hello world'
+# def test_page_loader():
+#     assert run()  == 'Hello world'
+# def test_page_loader():
+#     assert run()  == 'Hello world'
+# def test_page_loader():
+#     assert run()  == 'Hello world'
+# def test_page_loader():
+#     assert run()  == 'Hello world'

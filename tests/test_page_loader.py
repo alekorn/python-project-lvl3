459111fb249df43748e442ecc0fd7a43b977250f
alekorn <entkorn@gmail.com>
2020-03-15 23:06:29 +0300

    add arg_parse and page_loader tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,8 +1,7 @@
-from page_loader.engine import run, give_a_name
-
-
-def test_page_loader():
-    assert run()  == 'Hello world'
+from page_loader.engine import give_a_name, arg_parse, page_loader
+import sys
+import tempfile
+import os
 
 
 def test_give_a_name():
@@ -12,16 +11,21 @@ def test_give_a_name():
     assert give_a_name('https://hexlet.io/A1234567890')  == 'hexlet-io-A1234567890.html'
 
 
+def test_arg_parse():
+    args = arg_parse(['-o=/tmp/test/', 'https://hexlet.io/courses'])
+    assert args.url == 'https://hexlet.io/courses'
+    assert args.output == '/tmp/test/'
 
-# def test_page_loader():
-#     assert run()  == 'Hello world'
-# def test_page_loader():
-#     assert run()  == 'Hello world'
-# def test_page_loader():
-#     assert run()  == 'Hello world'
-# def test_page_loader():
-#     assert run()  == 'Hello world'
-# def test_page_loader():
-#     assert run()  == 'Hello world'
-# def test_page_loader():
-#     assert run()  == 'Hello world'
+
+def test_page_loader():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        page_loader(tmp_dir, 'https://hexlet.io/courses')
+        assert os.path.exists(f'{tmp_dir}/hexlet-io-courses.html')
+
+
+# def test_text_resp():
+#     pass
+#
+#
+# def test_file_save():
+#     pass

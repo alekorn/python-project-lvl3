a04590cb8950defc5a50ac957a63f1e0989e63c3
alekorn <entkorn@gmail.com>
2020-03-25 21:18:12 +0300

    add page_load tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,14 +1,14 @@
-from page_loader.engine import give_a_name, arg_parse, page_loader
+from page_loader.engine import normalize_url, arg_parse, page_load
 import sys
 import tempfile
 import os
 
 
-def test_give_a_name():
-    assert give_a_name('https://hexlet.io/courses')  == 'hexlet-io-courses.html'
-    assert give_a_name('http://hexlet.io/courses')  == 'hexlet-io-courses.html'
-    assert give_a_name('hexlet.io/courses')  == 'hexlet-io-courses.html'
-    assert give_a_name('https://hexlet.io/A1234567890')  == 'hexlet-io-A1234567890.html'
+def test_normalize_url():
+    assert normalize_url('https://hexlet.io/courses')  == 'hexlet-io-courses'
+    assert normalize_url('http://hexlet.io/courses')  == 'hexlet-io-courses'
+    assert normalize_url('hexlet.io/courses.html')  == 'hexlet-io-courses.html'
+    assert normalize_url('https://hexlet.io/A1234567890.html')  == 'hexlet-io-A1234567890.html'
 
 
 def test_arg_parse():
@@ -17,14 +17,9 @@ def test_arg_parse():
     assert args.output == '/tmp/test/'
 
 
-def test_page_loader():
+def test_page_load():
     with tempfile.TemporaryDirectory() as tmp_dir:
-        page_loader(tmp_dir, 'https://hexlet.io/courses')
-        assert os.path.exists(f'{tmp_dir}/hexlet-io-courses.html')
-
-
-def test_page_loader2():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        page_loader(tmp_dir, 'https://hexlet.io/courses')
-        assert os.path.exists(f'{tmp_dir}/hexlet-io-courses-files/')
-        assert os.path.exists(f'{tmp_dir}/hexlet-io-courses-files/cdn2-hexlet-io-assets-favicon-8fa102c058afb01de5016a155d7db433283dc7e08ddc3c4d1aef527c1b8502b6.ico"')
+        page_load(tmp_dir, 'https://ru.hexlet.io/courses')
+        assert os.path.exists(f'{tmp_dir}/ru-hexlet-io-courses.html')
+        assert os.path.exists(f'{tmp_dir}/ru-hexlet-io-courses_files/')
+        assert os.path.exists(f'{tmp_dir}/ru-hexlet-io-courses_files/ru-hexlet-io-lessons.rss')

67d3cff89def9bcd36df311a162655767a00a353
alekorn <entkorn@gmail.com>
2020-04-02 10:37:08 +0300

    fix flake8 warnings

--- page_loader/script/page_loader.py
+++ page_loader/script/page_loader.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 import sys
-import os
 from page_loader.engine import page_load, arg_parse
 
 
 def main():
     args = arg_parse(sys.argv[1:])
     page_load(args.output, args.url)
-    #file_name = normalize_url(args.url)
-    #text = get_text_resp(args.url)
-    #file_save(os.path.join(args.output, file_name), text)
 
 
 if __name__ == '__main__':

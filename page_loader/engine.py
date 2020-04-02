8fe992cb8038ade7864d4024c27c768711d3e55f
alekorn <entkorn@gmail.com>
2020-04-02 10:38:25 +0300

    add page_load and refactoring old func

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -2,6 +2,7 @@ import argparse
 import requests
 import re
 import os
+import copy
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
@@ -22,31 +23,32 @@ def arg_parse(argv):
 
 
 def page_load(output, url):
-    parse_url = urlparse(url)
-    file_name = normalize_url(url) + '.html'
-    dir_name = output + '/' + normalize_url(url) + '_files'
+    file_name = f'{normalize_url(url)}.html'
+    dir_name = f'{normalize_url(url)}_files'
+    dir_path = f'{output}/{dir_name}'
     text = requests.get(url).text
     soup = BeautifulSoup(text, 'html.parser')
-    file_save(os.path.join(output, file_name), text, 'w')
-    attr_texts = soup_attr_find(soup, TAGS_ATTRS)
-    print(*attr_texts) # debug
-    os.mkdir(dir_name)
+    attr_texts, new_soup = soup_attr_find(soup, TAGS_ATTRS, dir_name)
+    file_save(os.path.join(output, file_name), str(new_soup), 'w')
+    os.mkdir(dir_path)
     for attr_text in attr_texts:
         parse_attr = urlparse(attr_text)
         if parse_attr.scheme:
             data = requests.get(attr_text)
         else:
-            data = requests.get(parse_url.scheme + '://' + parse_url.netloc + attr_text)
-        file_save(dir_name + '/' + normalize_url(attr_text), data.content, 'wb') # change 3 arg
+            data = requests.get(f'{url}/{attr_text}')
+        file_save(f'{dir_path}/{normalize_url(attr_text)}', data.content, 'wb')
 
 
-def soup_attr_find(soup, tag_attr_dict):
+def soup_attr_find(soup, tag_attr_dict, dir_name):
+    new_soup = copy.copy(soup)
     result_list = []
     for key, value in tag_attr_dict.items():
-        for link in soup.find_all(key):
-            if link.get(value):
-                result_list.append(link.get(value))
-    return result_list
+        for link in new_soup.find_all(key):
+            if value in link.attrs:
+                result_list.append(link[value])
+                link[value] = dir_name + '/' + normalize_url(link[value])
+    return result_list, new_soup
 
 
 def file_save(file_path, data, data_type):
@@ -54,21 +56,11 @@ def file_save(file_path, data, data_type):
         output_file.write(data)
 
 
-# def soup_attr_finder_old(soup, tag, attr):
-#     result_list = []
-#     for link in soup.find_all(tag):
-#         if link.get(attr):
-#             result_list.append(link.get(attr))
-#     return result_list
-
-
 def normalize_url(url):
     parsed_url = urlparse(url)
-    if '.' in parsed_url.path:
-        path, ext = parsed_url.path.rsplit('.', 1)
-        ext = '.' + ext
-    else:
-        path = parsed_url.path
-        ext = ''
-    normalized_path = re.sub(r'[^a-zA-Z0-9]', '-', parsed_url.netloc + path)
+    file_name, ext = os.path.splitext(parsed_url.path)
+    normalized_path = re.sub(
+            r'[^a-zA-Z0-9]',
+            '-', parsed_url.netloc + file_name
+            )
     return normalized_path.strip('-') + ext

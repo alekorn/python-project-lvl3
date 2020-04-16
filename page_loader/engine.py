12a997bc0e6e4b1a7447d1036e8962a33d9c912a
alekorn <entkorn@gmail.com>
2020-04-16 13:44:25 +0300

    add logging and refactoring

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -2,9 +2,9 @@ import argparse
 import requests
 import re
 import os
-import copy
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from page_loader.logger import LOGGER
 
 TAGS_ATTRS = {'link': 'href', 'script': 'src', 'img': 'src'}
 
@@ -12,6 +12,14 @@ TAGS_ATTRS = {'link': 'href', 'script': 'src', 'img': 'src'}
 def arg_parse(argv):
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('url', type=str, help='')
+    parser.add_argument(
+        '-l',
+        '--log',
+        help='',
+        type=str,
+        choices=['debug', 'info', 'warning', 'error', 'critical'],
+        default='warning'
+    )
     parser.add_argument(
         '-o',
         '--output',
@@ -23,32 +31,43 @@ def arg_parse(argv):
 
 
 def page_load(output, url):
+    LOGGER.info('TEST this is INFO')
     file_name = f'{normalize_url(url)}.html'
     dir_name = f'{normalize_url(url)}_files'
     dir_path = f'{output}/{dir_name}'
-    text = requests.get(url).text
-    soup = BeautifulSoup(text, 'html.parser')
-    attr_texts, new_soup = soup_attr_find(soup, TAGS_ATTRS, dir_name)
+    text = get_html(url)
+    attr_texts, new_soup = get_data(text, TAGS_ATTRS, dir_name)
     file_save(os.path.join(output, file_name), str(new_soup), 'w')
     os.mkdir(dir_path)
-    for attr_text in attr_texts:
+    save_content(attr_texts, dir_path, url)
+
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+
+def get_data(text, tag_attr_dict, dir_name):
+    LOGGER.error('TEST this is ERROR')
+    soup = BeautifulSoup(text, 'html.parser')
+    result_list = []
+    for key, value in tag_attr_dict.items():
+        for link in soup.find_all(key):
+            if value in link.attrs:
+                result_list.append(link[value])
+                link[value] = dir_name + '/' + normalize_url(link[value])
+    return result_list, soup
+
+
+def save_content(content_list, dir_path, url):
+    for attr_text in content_list:
         parse_attr = urlparse(attr_text)
         if parse_attr.scheme:
             data = requests.get(attr_text)
         else:
             data = requests.get(f'{url}/{attr_text}')
         file_save(f'{dir_path}/{normalize_url(attr_text)}', data.content, 'wb')
-
-
-def soup_attr_find(soup, tag_attr_dict, dir_name):
-    new_soup = copy.copy(soup)
-    result_list = []
-    for key, value in tag_attr_dict.items():
-        for link in new_soup.find_all(key):
-            if value in link.attrs:
-                result_list.append(link[value])
-                link[value] = dir_name + '/' + normalize_url(link[value])
-    return result_list, new_soup
+    LOGGER.critical('TEST this is CRITICAL')
 
 
 def file_save(file_path, data, data_type):
--- page_loader/script/page_loader.py
+++ page_loader/script/page_loader.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import sys
 from page_loader.engine import page_load, arg_parse
+from page_loader.logger import LOGGER
 
 
 def main():
     args = arg_parse(sys.argv[1:])
+    LOGGER.setLevel(args.log.upper())
+    LOGGER.info('Started')
     page_load(args.output, args.url)
+    LOGGER.debug('Finished')
 
 
 if __name__ == '__main__':

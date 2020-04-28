494a9833bcfcb76b9eeaf5b5542f5d3049b8ac7f
alekorn <entkorn@gmail.com>
2020-04-28 09:27:49 +0300

    remove comments

--- page_loader/logger.py
+++ page_loader/logger.py
@@ -2,7 +2,6 @@ import logging.config
 
 LOGGER_CONFIG = {
         'version': 1,
-        # 'disable_existing_loggers': True,
         'formatters': {
             'file_formatter': {
                 'format': '{asctime} | {levelname} | {message}',
@@ -16,7 +15,6 @@ LOGGER_CONFIG = {
         'handlers': {
             'console_handler': {
                 'class': 'logging.StreamHandler',
-                # 'stream': 'sys.stderr',
                 'level': 'WARNING',
                 'formatter': 'console_formatter'
                 },
@@ -32,16 +30,10 @@ LOGGER_CONFIG = {
             'my_logger': {
                 'level': 'DEBUG',
                 'handlers': ['file_handler', 'console_handler'],
-                # 'propagate': False
                 }
-            },
-        # 'filters': {},
-        # 'root': {},
-        # 'incrimental': True
+            }
         }
 
 
 logging.config.dictConfig(LOGGER_CONFIG)
 LOGGER = logging.getLogger('my_logger')
-# LOGGER.setLevel('CRITICAL') # TEST
-# logger.setLevel(args.logging_level.upper())
